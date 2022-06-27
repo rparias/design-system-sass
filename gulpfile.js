c361b4aef07d650355c41e66912ab0e494662f43
Ronald Arias <ariasron@hotmail.com>
2022-06-27 11:43:39 -0500

    installs gulp-purgecss to remove the size of the css generated file better for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp'
 import gulpSass from 'gulp-sass'
 import nodeSass from 'sass'
+import purgecss from 'gulp-purgecss'
 
 const { src, dest, watch, series } = gulp
 const sass = gulpSass(nodeSass)
@@ -8,11 +9,12 @@ const sass = gulpSass(nodeSass)
 const buildStyles = () => {
   return src('shinobi/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
 const watchTask = () => {
-  watch(['shinobi/**/*.scss'], buildStyles)
+  watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 export default series(buildStyles, watchTask)
